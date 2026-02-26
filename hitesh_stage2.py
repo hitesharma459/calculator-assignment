4712e196cfd658cb5f8d5f75954a682b93b39b91
hitesharma459 <fcdhs96850@gmail.com>
2026-02-26 18:51:54 +0530

    Add files via upload

---
+++ hitesh_stage2.py
@@ -0,0 +1,36 @@
+# Stage 2 Calculator with result check
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero")
+        exit()
+    else:
+        result = num1 / num2
+
+else:
+    print("Invalid operator")
+    exit()
+
+print("Result =", result)
+
+if result > 0:
+    print("Positive")
+
+elif result < 0:
+    print("Negative")
+
+else:
+    print("Zero")
